26a87fd2c53f39bacdc803b1216ac8d7e29d0500
Julio C <julio.dzalt@gmail.com>
2025-01-11 02:49:45 -0600

    added chnges

---
+++ README.md
@@ -0,0 +1,3 @@
+qmk compile -kb sofle_choc -km jdiazaltamir_choc_sofle
+
+qmk flash -kb sofle_choc -km jdiazaltamir_choc_sofle
--- keymap.c
+++ keymap.c
@@ -16,7 +16,6 @@
 
 #include QMK_KEYBOARD_H
 
-static uint16_t last_keycode = 0;
 
 #ifdef OLED_ENABLE
 
@@ -72,81 +71,80 @@ static void print_status_2(void) {
 
  bool oled_task_user(void) {
      if (is_keyboard_master()) {
-         print_status_narrow();
+        print_status_narrow();
      } else {
-         print_status_2();
+        print_status_2();
+        //oled_draw_bitmap(0, 0, qmk_logo); // Esto debería mostrar el logo QMK por defecto
+
      }
      return false;
  }
 
 #endif
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        // Se presionó la tecla
-        last_keycode = keycode;
-    }
-    return true;  // Siempre retornar true para continuar el procesamiento
-}
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+       // Layer 0: Default Layer (QWERTY)
+    // ,-----------------------------------------.                    ,-----------------------------------------.
+    // |  ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Bspc |
+    // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    // |  Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
+    // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+    // | Caps  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+    // |------+------+------+------+------+------|  Mute |    | MPlay  |------+------+------+------+------+------|
+    // |LShift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
+    // `-----------------------------------------/       /     \      \-----------------------------------------'
+    //              | LCTL | LGUI | LALT | MO(1)| / Space /       \ Enter \  | MO(2)|   [  |   ]  |   \   |
+
+    // Layer 0: Default Layer (QWERTY)
     [0] = LAYOUT(
-        // Layer 0: Default Layer (QWERTY)
-        // ,-----------------------------------------.                    ,-----------------------------------------.
-        // |  ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Bspc |
-        // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-        // |  Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
-        // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL,
-        // | Caps  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
-        // |------+------+------+------+------+------|  Mute |    | MPlay  |------+------+------+------+------+------|
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,                               KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        // |LShift |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  -   |
-        // `-----------------------------------------/       /     \      \-----------------------------------------'
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE,             KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-        //              | LCTL | LGUI | LALT | MO(1)| / Space /       \ Enter \  | MO(2)|   [  |   ]  |   \   |
-        //              |      |      |      |      |/       /         \       \ |      |      |      |      |
-        KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC,                          KC_ENT, MO(2), KC_LBRC, KC_RBRC, KC_BSLS),
-    [1] = LAYOUT(
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC,                          KC_ENT, MO(2), KC_LBRC, KC_RBRC, KC_BSLS
+    ),
+
     // Layer 1: Function Layer
     // ,-----------------------------------------.                    ,-----------------------------------------.
     // |  `~  |  F1  |  ---  |  ---  |  ---  |  ---  |                    |  ---  |  ---  |  F8  |  F9  |  KC_F10  |  KC_F11  |
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F10,
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |                    |  ---  |  UP  |  ---  |  ---  |  ---  |  ---  |
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO,
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |-------.    ,-------|  LEFT  |  DOWN  |  RIGHT  |  ---  |  ---  |  ---  |
     // |------+------+------+------+------+------|  ---  |    |  ---  |------+------+------+------+------+------|
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO,
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |-------|    |-------|  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |
     // `-----------------------------------------/       /     \      \-----------------------------------------'
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //              |  ---  |  ---  |  ---  |Trans | /  ---  /       \  ---  \  |Trans |  ---  |  ---  |  ---  |
-    //              |       |       |       |      |/       /         \       \ |      |       |       |       |
-                     KC_NO, KC_NO, KC_NO, KC_TRNS,                  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
-),
-[2] = LAYOUT(
-    // Layer 2: Additional Layer
+    //              |  ---  |  ---  |  ---  |  Trans  | /  Trans  /       \  Trans  \  |MENU |  ---  |  ---  |  ---  |
+
+    // Layer 1: Function Layer
+    [1] = LAYOUT(
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_APP, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+      // Layer 2: Additional Layer
     // ,-----------------------------------------.                    ,-----------------------------------------.
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |                    |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |                    |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |
     // |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |-------.    ,-------|  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |
     // |------+------+------+------+------+------|  ---  |    |  ---  |------+------+------+------+------+------|
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     // |  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |-------|    |-------|  ---  |  ---  |  ---  |  ---  |  ---  |  ---  |
     // `-----------------------------------------/       /     \      \-----------------------------------------'
-         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    //              |  ---  |  ---  |  ---  |  ---  | /  ---  /       \  ---  \  |  ---  |  ---  |  ---  |  ---  |
-    //              |       |       |       |       |/       /         \       \ |       |       |       |       |
-                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
-),
+    //           |  ---  |  ---  |  ---  |  Trans  | /  Trans  /       \  Trans  \  |  Trans  |  ---  |  ---  |  ---  |
+
+    // Layer 2: Additional Layer
+    [2] = LAYOUT(
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_INS, KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_DEL,  KC_END,  KC_PGDN, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
 
 };
 
@@ -155,20 +153,20 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     // Capa 0: Configuración general (Volumen y pistas)
     [0] =
         {
-            ENCODER_CCW_CW(KC_VOLU, KC_VOLD),   // Encoder 1: Volumen arriba/abajo
-            ENCODER_CCW_CW(KC_MNXT, KC_MPRV)    // Encoder 2: Pista siguiente/anterior
+            ENCODER_CCW_CW(KC_VOLU, KC_VOLD),   // Encoder : Volumen arriba/abajo
+            ENCODER_CCW_CW(KC_MPLY, KC_MSTP)    // Encoder : Pista play/stop
         },
     // Capa 1: Configuración para el ratón (scroll vertical y horizontal)
     [1] =
         {
-            ENCODER_CCW_CW(MS_WHLU, MS_WHLD),    // Encoder 1: Desplazamiento hacia arriba/abajo
-            ENCODER_CCW_CW(MS_WHLL, MS_WHLR)   // Encoder 2: Desplazamiento lateral (derecha/izquierda)
+            ENCODER_CCW_CW(MS_WHLU, MS_WHLD),    // Encoder : Desplazamiento hacia arriba/abajo
+            ENCODER_CCW_CW(MS_WHLL, MS_WHLR)   // Encoder : Desplazamiento lateral (derecha/izquierda)
         },
     // Capa 2: Configuración de navegación (inicio y fin, scroll izquierda/derecha)
     [2] =
         {
-            ENCODER_CCW_CW(KC_END, KC_HOME),      // Encoder 2: Inicio/Fin del documento
-            ENCODER_CCW_CW(KC_WH_L, KC_WH_R)   // Encoder 1: Desplazamiento de página izquierda/derecha
+            ENCODER_CCW_CW(XXXXXXX,XXXXXXX ), 
+            ENCODER_CCW_CW(XXXXXXX,XXXXXXX )  
         },
 };
 #endif
