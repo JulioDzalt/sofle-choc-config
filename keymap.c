34480d517c98837cdb6279e2cb6f61071b55741b
Julio C <julio.dzalt@gmail.com>
2025-01-11 03:39:23 -0600

    added chnges

--- keymap.c
+++ keymap.c
@@ -98,9 +98,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Layer 0: Default Layer (QWERTY)
     [0] = LAYOUT(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL,
-        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,                               KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                                KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_EQL,
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G,                               KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE,             KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
         KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC,                          KC_ENT, MO(2), KC_LBRC, KC_RBRC, KC_BSLS
     ),
@@ -119,11 +119,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Layer 1: Function Layer
     [1] = LAYOUT(
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_APP, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                  KC_F6,   KC_F7,   KC_F8, KC_F9, KC_F10, KC_TRNS,
+        KC_TAB, KC_F11, KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, KC_TRNS,
+        KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS,
+        KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,     KC_TRNS, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS, KC_SPC,                                 KC_ENT, KC_APP, KC_TRNS, KC_TRNS, KC_TRNS
     ),
       // Layer 2: Additional Layer
     // ,-----------------------------------------.                    ,-----------------------------------------.
@@ -139,13 +139,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Layer 2: Additional Layer
     [2] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_INS, KC_HOME, KC_PGUP, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, KC_DEL,  KC_END,  KC_PGDN, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS, KC_TRNS,                  KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_INS,  KC_HOME, KC_PGUP, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                       KC_TRNS, KC_DEL,  KC_END,  KC_PGDN, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -154,7 +153,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =
         {
             ENCODER_CCW_CW(KC_VOLU, KC_VOLD),   // Encoder : Volumen arriba/abajo
-            ENCODER_CCW_CW(KC_MPLY, KC_MSTP)    // Encoder : Pista play/stop
+            ENCODER_CCW_CW(XXXXXXX, KC_MSTP)    // Encoder : Pista play/stop
         },
     // Capa 1: Configuración para el ratón (scroll vertical y horizontal)
     [1] =
@@ -165,8 +164,8 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     // Capa 2: Configuración de navegación (inicio y fin, scroll izquierda/derecha)
     [2] =
         {
-            ENCODER_CCW_CW(XXXXXXX,XXXXXXX ), 
-            ENCODER_CCW_CW(XXXXXXX,XXXXXXX )  
+            ENCODER_CCW_CW(MS_WHLU, MS_WHLD),    // Encoder : Desplazamiento hacia arriba/abajo
+            ENCODER_CCW_CW(MS_WHLL, MS_WHLR)   // Encoder : Desplazamiento lateral (derecha/izquierda)
         },
 };
 #endif
