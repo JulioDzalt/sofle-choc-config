9d4d504725ac5fb5e2ec41d7a02434039e544d13
Julio C <julio.dzalt@gmail.com>
2025-01-15 02:25:18 -0600

    added chnges

--- keymap.c
+++ keymap.c
@@ -23,9 +23,9 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
      if (is_keyboard_master()) {
          return OLED_ROTATION_270;
      }
-     if (!is_keyboard_master()) {
-         return OLED_ROTATION_180;
-     }
+    //  if (!is_keyboard_master()) {
+    //      return OLED_ROTATION_180;
+    //  }
 
      return rotation;
  }
@@ -58,22 +58,32 @@ static void print_status_narrow(void) {
     oled_write_ln_P(PSTR(" :D "), false);
 }
 
-static void print_status_2(void) {
-    oled_set_cursor(0,1);
+// static void print_status_2(void) {
+//     oled_set_cursor(0,1);
     
-    oled_write_ln_P(PSTR("Getsemani <3"), false);
+//     oled_write_ln_P(PSTR("Getsemani <3"), false);
 
-    oled_write_ln_P(PSTR("-----"), false);
-    oled_write_ln_P(PSTR("(=^_^=)"), false);
+//     oled_write_ln_P(PSTR("-----"), false);
+//     oled_write_ln_P(PSTR("(=^_^=)"), false);
 
+// }
+
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
 }
 
  bool oled_task_user(void) {
      if (is_keyboard_master()) {
         print_status_narrow();
      } else {
-        print_status_2();
-        //oled_draw_bitmap(0, 0, qmk_logo); // Esto debería mostrar el logo QMK por defecto
+        // render_logo();
+        render_logo(); // Esto debería mostrar el logo QMK por defecto
 
      }
      return false;
@@ -158,6 +168,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                 KC_TRNS,  KC_APP ,  KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 };
+
+
+
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
